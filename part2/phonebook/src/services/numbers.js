d9054c6d5ffcf6ffc3017d7d817be85b2993fe31
endless_suffering <nocturne6667@gmail.com>
2020-07-07 04:49:44 +0300

    added api

--- part2/phonebook/src/services/numbers.js
+++ part2/phonebook/src/services/numbers.js
@@ -1,28 +1,28 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "http://localhost:3001/api/persons";
 
 const getAll = () => {
-    const req = axios.get(baseUrl);
-    return req.then(res => res.data);
+  const req = axios.get(baseUrl);
+  return req.then((res) => res.data);
 };
 
 const create = (newObj) => {
-    const req = axios.post(baseUrl, newObj);
-    return req.then(res => res.data);
+  const req = axios.post(baseUrl, newObj);
+  return req.then((res) => res.data);
 };
 
 const del = (id) => {
-    const req = axios.delete(`${baseUrl}/${id}`);
-    return req.then(res => res.data);
-}
+  const req = axios.delete(`${baseUrl}/${id}`);
+  return req.then((res) => res.data);
+};
 const update = (id, newObj) => {
-    const req = axios.put(`${baseUrl}/${id}`, newObj);
-    return req.then(res => res.data)
-}
+  const req = axios.put(`${baseUrl}/${id}`, newObj);
+  return req.then((res) => res.data);
+};
 export default {
-    getAll,
-    create,
-    del,
-    update
+  getAll,
+  create,
+  del,
+  update,
 };
