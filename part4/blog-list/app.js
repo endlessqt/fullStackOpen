d02ddadbb5822328d6d9de61fec443d29a28431a
endless_suffering <nocturne6667@gmail.com>
2020-07-11 22:48:30 +0300

    added logger

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -1,43 +1,43 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const logger = require("./utils/logger");
 
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
-})
+  likes: Number,
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = 'mongodb+srv://test1:testtest@clustertest.0nfqq.mongodb.net/blog-list?retryWrites=true&w=majority'
+const mongoUrl =
+  "mongodb+srv://test1:testtest@clustertest.0nfqq.mongodb.net/blog-list?retryWrites=true&w=majority";
 
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
 
-const PORT = 3003
+const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  logger.info(`Server running on port ${PORT}`);
+});
