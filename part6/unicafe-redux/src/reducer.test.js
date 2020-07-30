2b11f8f52f2f5cf54c98d64175587e6fab785976
endless_suffering <nocturne6667@gmail.com>
2020-07-30 20:05:29 +0300

    cloned app + tests for reducer

---
+++ part6/unicafe-redux/src/reducer.test.js
@@ -0,0 +1,84 @@
+import deepFreeze from "deep-freeze";
+import counterReducer from "./reducer";
+
+describe("unicafe reducer", () => {
+  const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0,
+  };
+
+  test("should return a proper initial state when called with undefined state", () => {
+    const state = {};
+    const action = {
+      type: "DO_NOTHING",
+    };
+
+    const newState = counterReducer(undefined, action);
+    expect(newState).toEqual(initialState);
+  });
+
+  test("good is incremented", () => {
+    const action = {
+      type: "GOOD",
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 1,
+      ok: 0,
+      bad: 0,
+    });
+  });
+  test("bad is incremented", () => {
+    const action = {
+      type: "BAD",
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1,
+    });
+  });
+  test("ok is incremented", () => {
+    const action = {
+      type: "OK",
+    };
+    const state = initialState;
+
+    deepFreeze(state);
+    const newState = counterReducer(state, action);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0,
+    });
+  });
+  test("zero is turning state in initial pos", () => {
+    const action = {
+      type: "ZERO",
+    };
+    const state = {
+      good: 100,
+      bad: 25,
+      ok: 11,
+    };
+
+    deepFreeze(state);
+
+    const newState = counterReducer(state, action);
+
+    expect(newState).toEqual(initialState);
+    expect(newState).not.toContain({
+      good: 100,
+      bad: 25,
+      ok: 11,
+    });
+  });
+});
