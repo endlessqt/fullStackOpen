22538c203c59841af34ee689821bc6705ae22fc7
endless_suffering <nocturne6667@gmail.com>
2020-08-19 00:17:03 +0300

    added blogs style

--- part7/bloglist-part7/src/components/Blogs.js
+++ part7/bloglist-part7/src/components/Blogs.js
@@ -1,20 +1,32 @@
 import React from "react";
+import { Stack, Divider, PseudoBox, Tooltip } from "@chakra-ui/core";
 import { Link } from "react-router-dom";
 function Blogs({ blogs }) {
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: "solid",
-    borderWidth: 1,
-    marginBottom: 5,
-  };
-  return blogs.map((blog) => {
-    return (
-      <div style={blogStyle} key={blog.id}>
-        <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
-      </div>
-    );
-  });
+  return (
+    <>
+      <Stack mx={10} mt={5}>
+        {blogs.map((blog) => {
+          return (
+            <Tooltip
+              key={blog.id}
+              placement="top"
+              bg="black"
+              label="Click for more info"
+              hasArrow>
+              <PseudoBox
+                fontSize={["base", "lg", "xl", "2xl"]}
+                textAlign="center"
+                color="black"
+                _hover={{ color: "gray.500" }}>
+                <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
+                <Divider borderColor="gray.500" my={5} />
+              </PseudoBox>
+            </Tooltip>
+          );
+        })}
+      </Stack>
+    </>
+  );
 }
 
 export default Blogs;
