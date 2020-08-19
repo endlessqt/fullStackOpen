6b264e593a27c9734810441220c1df424eedff7b
endless_suffering <nocturne6667@gmail.com>
2020-08-19 19:36:02 +0300

    added notification styles

--- part7/bloglist-part7/src/components/Notification.js
+++ part7/bloglist-part7/src/components/Notification.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
+import { Alert, AlertTitle, AlertIcon } from "@chakra-ui/core";
 
 const Notification = () => {
   const notification = useSelector((state) => state.notification);
+
   if (notification === null) {
     return null;
   }
-  return <div>{notification.text}</div>;
+  return (
+    <Alert status="success">
+      <AlertIcon />
+      <AlertTitle>{notification.text}</AlertTitle>
+    </Alert>
+  );
 };
 
 export default Notification;
